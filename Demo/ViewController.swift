ee9da520d1f64d57ead01d9afd6ee287f5692969
Daniel Langh <intonarumori@gmail.com>
2015-12-22 01:39:08 +0100

    updates

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -10,16 +10,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var textField:WatermarkedTextField?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        textField?.placeholderLabel.font = UIFont.italicSystemFontOfSize(18)
+    }
+    
+    @IBAction func addError() {
+        self.textField?.errorMessage = "error message"
+    }
+    
+    @IBAction func selectedTitleChanged(segmentedControl:UISegmentedControl) {
+        switch segmentedControl.selectedSegmentIndex {
+            case 0:
+                self.textField?.selectedTitle = nil
+            case 1:
+                self.textField?.selectedTitle = "Selected title"
+            default:
+                break
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func deselectedTitleChanged(segmentedControl:UISegmentedControl) {
+        switch segmentedControl.selectedSegmentIndex {
+        case 0:
+            self.textField?.deselectedTitle = nil
+        case 1:
+            self.textField?.deselectedTitle = "Deselected title"
+        default:
+            break
+        }
     }
-
-
+    
+    @IBAction func placeholderChanged(segmentedControl:UISegmentedControl) {
+        switch segmentedControl.selectedSegmentIndex {
+        case 0:
+            self.textField?.placeholder = nil
+        case 1:
+            self.textField?.placeholder = "Placeholder"
+        default:
+            break
+        }
+    }
 }
 
