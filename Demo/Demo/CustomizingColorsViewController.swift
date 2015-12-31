c454dbddd5de74579908158b48e938d6626bd500
Daniel Langh <intonarumori@gmail.com>
2015-12-31 16:39:07 +0100

    added color example toggles

--- Demo/Demo/CustomizingColorsViewController.swift
+++ Demo/Demo/CustomizingColorsViewController.swift
@@ -10,4 +10,44 @@ import UIKit
 
 class CustomizingColorsViewController: UIViewController {
 
+    @IBOutlet weak var textField:WatermarkedTextField?
+    
+    @IBAction func addError() {
+        self.textField?.errorMessage = "error message"
+    }
+    
+    @IBAction func resignTextField() {
+        self.textField?.resignFirstResponder()
+    }
+    
+    @IBAction func titleColorChanged(segmentedControl:UISegmentedControl) {
+        self.textField?.titleColor = self.colorForSegmentIndex(segmentedControl.selectedSegmentIndex)
+    }
+
+    @IBAction func selectedTitleColorChanged(segmentedControl:UISegmentedControl) {
+        self.textField?.selectedTitleColor = self.colorForSegmentIndex(segmentedControl.selectedSegmentIndex)
+    }
+
+    @IBAction func placeholderColorChanged(segmentedControl:UISegmentedControl) {
+        self.textField?.placeholderColor = self.colorForSegmentIndex(segmentedControl.selectedSegmentIndex)
+    }
+
+    @IBAction func textColorChanged(segmentedControl:UISegmentedControl) {
+        self.textField?.textColor = self.colorForSegmentIndex(segmentedControl.selectedSegmentIndex)
+    }
+
+    @IBAction func errorColorChanged(segmentedControl:UISegmentedControl) {
+        self.textField?.errorColor = self.colorForSegmentIndex(segmentedControl.selectedSegmentIndex)
+    }
+
+    // MARK: helper
+    
+    func colorForSegmentIndex(segmentIndex:Int) -> UIColor {
+        switch segmentIndex {
+        case 0: return UIColor.whiteColor()
+        case 1: return UIColor.redColor()
+        case 2: return UIColor.blueColor()
+        default: return UIColor.blackColor()
+        }
+    }
 }
