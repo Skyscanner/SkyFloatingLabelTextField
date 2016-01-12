fdab70717888f9926cbe911b34591447e3ff8b11
Gergely Orosz <gergely.orosz@gmail.com>
2016-01-12 17:37:09 +0000

    Updating the license with the Apache license

--- SkyFloatingLabelTextField/SkyFloatingLabelTextField/SkyFloatingLabelTextField.swift
+++ SkyFloatingLabelTextField/SkyFloatingLabelTextField/SkyFloatingLabelTextField.swift
@@ -1,10 +1,10 @@
+//  Copyright 2016 Skyscanner Ltd
 //
-//  SkyFloatingLabelTextField.swift
-//  Demo
+//  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
 //
-//  Created by Daniel Langh on 21/12/15.
-//  Copyright © 2015 Skyscanner. All rights reserved.
+//  http://www.apache.org/licenses/LICENSE-2.0
 //
+//  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
 
 import UIKit
 
@@ -119,7 +119,7 @@ public class SkyFloatingLabelTextField: UIControl, UITextFieldDelegate {
         Identifies whether the text object should hide the text being entered.
     */
     public var secureTextEntry:Bool = false {
-        didSet {
+        didSet { 
             self.textField.secureTextEntry = secureTextEntry
             self.textField.fixCaretPosition()
         }
