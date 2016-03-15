960c6188f6aabd5100d66e4cbf5e2de69c3c987b
Daniel Langh <intonarumori@gmail.com>
2016-03-15 19:19:29 +0100

    fixed iconlabel example

--- SkyFloatingLabelTextField/SkyFloatingLabelTextFieldExample/Example4/IconTextField.swift
+++ SkyFloatingLabelTextField/SkyFloatingLabelTextFieldExample/Example4/IconTextField.swift
@@ -12,7 +12,7 @@ import UIKit
 public class IconTextField: SkyFloatingLabelTextField {
     
     public var iconLabel:UILabel!
-    public var iconWidth:CGFloat = 20.0
+    public var iconWidth:CGFloat = 25.0
     
     @IBInspectable
     public var icon:String? {
@@ -38,7 +38,6 @@ public class IconTextField: SkyFloatingLabelTextField {
     func createIconLabel() {
         let iconLabel = UILabel()
         iconLabel.backgroundColor = UIColor.clearColor()
-        iconLabel.font = UIFont(name: "Dashicons-Regular", size: 15)
         iconLabel.textAlignment = .Center
         iconLabel.autoresizingMask = [.FlexibleTopMargin, .FlexibleRightMargin]
         self.iconLabel = iconLabel
@@ -65,6 +64,12 @@ public class IconTextField: SkyFloatingLabelTextField {
     // MARK: Custom layout overrides
     
     override public func textRectForBounds(bounds: CGRect) -> CGRect {
+        var rect = super.textRectForBounds(bounds)
+        rect.origin.x += iconWidth
+        rect.size.width -= iconWidth
+        return rect
+    }
+    override public func editingRectForBounds(bounds: CGRect) -> CGRect {
         var rect = super.textRectForBounds(bounds)
         rect.origin.x += iconWidth
         rect.size.width -= iconWidth
