0ac6ae1b6e18e8fb0472bd7fa80c04c40cca227a
Hugo Tunius <h@tunius.se>
2019-01-30 09:32:32 +0000

    [Copyright] Update copyright notices

--- SkyFloatingLabelTextField/SkyFloatingLabelTextFieldExample/ViewController.swift
+++ SkyFloatingLabelTextField/SkyFloatingLabelTextFieldExample/ViewController.swift
@@ -1,4 +1,4 @@
-//  Copyright 2016-2017 Skyscanner Ltd
+//  Copyright 2016-2019 Skyscanner Ltd
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
